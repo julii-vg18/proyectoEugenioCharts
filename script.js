b2d2957283aa93944ddc3471d1bd69e785d080cd
Julian Villanueva <juliivg18@gmail.com>
2019-12-17 12:18:44 +0100

    Añadido tabla barras

--- script.js
+++ script.js
@@ -52,26 +52,61 @@ trataDatos(seleccion.options[seleccion.selectedIndex].value);
 seleccion.addEventListener('change', function () {
     var e = seleccion;
     var selected = e.options[e.selectedIndex].value;
+    vaciarCanvas()
     trataDatos(selected)
+
 });
 
 //Trata los datos de la opcion seleccionadas
 var mes;
 var semanas;
 function trataDatos(e) {
+    let color;
     //? Dependiendo del mes, este toma unos valores u otros
     console.log('Ha selecionado el mes: ' + e)
     if (e == "enero") {
+        color = 'red';
         mes = meses[0];
         semanas = mes.semanas;
+        pintaBarra(semanas, color)
         console.log(semanas)
     } else if (e == "febrero") {
+        color = 'green';
         mes = meses[1];
         semanas = mes.semanas;
-        console.log(semanas)
+        pintaBarra(semanas, color);
+        console.log(semanas);
     } else {
+        color = 'blue';
         mes = meses[2];
         semanas = mes.semanas;
+        pintaBarra(semanas, color);
         console.log(semanas)
     }
+}
+
+function pintaBarra(semanas, color) {
+    if (semanas.length = 4) {
+        let cont = 20
+        ctx.beginPath();
+
+        semanas.forEach((e) => {
+            let sab = e.sab;
+            let dom = e.dom;
+            ctx.fillStyle = color;
+            cont = cont + 100
+            //          orX    orY  w    h
+            ctx.fillRect(cont, 599, -40, -sab);
+            cont = cont + 50;
+            ctx.fillRect(cont, 599, -40, -dom);
+        });
+        ctx.closePath()
+
+    } else if (semanas.length = 5) {
+
+    }
+}
+
+function vaciarCanvas() {
+    ctx.clearRect(51, 599, 599, -600);
 }
